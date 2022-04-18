027c30e61c58f72b6eb5539b6c5c8eec426c56fb
TUNGEE_chen <billchen@tungee.com>
2022-04-18 18:39:04 +0800

    add headers

--- temp/generate_test_staticitis.py
+++ temp/generate_test_staticitis.py
@@ -1,3 +1,6 @@
+import platform
+import sys
+
 import pandas as pd
 import openpyxl
 
@@ -5,6 +8,7 @@ init_data = {
     'case_id': [1],
     'model_name': ['model name'],
     'url': ['api url which prepare to test'],
+    'headers': ['request\'s headers'],
     'case_title': ['case title'],
     'method': ['request method, get/post/put/delete...'],
     'payload': ['request data or params'],
@@ -12,6 +16,10 @@ init_data = {
     'actual': ['actual result'],
     'test_result': ['test result']
 }
+if platform.system() == 'Windows':
+    my_path = 'C:/Users/TUNGEE/Desktop/api_test_excel_template.xlsx'
+elif platform.system() == 'Mac':
+    my_path = '/Users/chenyw/Desktop/小陈的工作做不完/learning/api_test_excel_template.xlsx'
 df = pd.DataFrame(init_data)
-df.to_excel('/Users/chenyw/Desktop/小陈的工作做不完/learning/api_test_excel_template.xlsx', index=False)
+df.to_excel(my_path, index=False)
 print(df)
--- test_cases/test_login_ddt.py
+++ test_cases/test_login_ddt.py
@@ -1,4 +1,5 @@
 import json
+import platform
 import unittest
 
 import ddt
@@ -9,31 +10,37 @@ from common.excel_handler import ExcelHandler
 
 @ddt.ddt
 class TestLoginDdt(unittest.TestCase):
-    excel = ExcelHandler('/Users/chenyw/Desktop/小陈的工作做不完/learning/api_test_excel_template.xlsx')
+    if platform.system() == 'Windows':
+        my_path = 'C:/Users/TUNGEE/Desktop/api_test_excel_template.xlsx'
+    elif platform.system() == 'Mac':
+        my_path = '/Users/chenyw/Desktop/小陈的工作做不完/learning/api_test_excel_template.xlsx'
+
+    excel = ExcelHandler(my_path)
     case_data = excel.read_excel('Sheet1')
     print(case_data)
 
     def setUp(self) -> None:
         self.req = RequestHandler()
+        self.my_path = TestLoginDdt.my_path
 
     def tearDown(self) -> None:
         self.req.close_session()
 
     @ddt.data(*case_data)
     def test_login_success(self, items):
-        res = self.req.visit(method=items['method'], url=items['url'], data=items['payload'],
-                             headers=items['header'])  # excel表缺一个header字段
+        res = self.req.visit(method=items['method'], url=items['url'], data=eval(items['payload']),
+                             headers=eval(items['headers']))
         try:
-            self.assertEqual(res.status_code, items['expect'][0])
+            self.assertEqual(res.status_code, items['expected'])
             result = 'pass'
         except AssertionError as e:
             result = 'fail'
             raise e
         finally:
-            TestLoginDdt.excel.write_excel(file='/Users/chenyw/Desktop/小陈的工作做不完/learning/api_test_excel_template.xlsx',
+            TestLoginDdt.excel.write_excel(file=self.my_path,
                                            sheet_name='Sheet1', row=items['case_id'] + 1, column=9,
                                            data=res.status_code)
-            TestLoginDdt.excel.write_excel(file='/Users/chenyw/Desktop/小陈的工作做不完/learning/api_test_excel_template.xlsx',
+            TestLoginDdt.excel.write_excel(file=self.my_path,
                                            sheet_name='Sheet1', row=items['case_id'] + 1, column=10,
                                            data=result)
 
